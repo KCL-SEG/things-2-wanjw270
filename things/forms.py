0c6c3e264de4a39906e048c2ab905c369858d9f0
Jingyi Wang <3072019561@qq.com>
2022-11-20 09:40:51 +0000

    commit2

--- things/forms.py
+++ things/forms.py
@@ -7,4 +7,4 @@ class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
         fields = {'name', 'description', 'quantity'}
-        widgets = {'description': forms.Textarea(), 'quantity':forms.NumberINput()}
+        widgets = {'description': forms.Textarea(), 'quantity':forms.NumberInput()}
